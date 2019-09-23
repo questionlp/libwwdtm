402b90c5467f8b50d9da5034cc2cec92f8e9bff1
Linh Pham <dev@wwdt.me>
2019-09-23 13:44:48 -0700

    Standing up new module files

---
+++ wwdtm/guest/details.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018-2019 Linh Pham
+# wwdtm is relased under the terms of the Apache License 2.0
+"""This module provides functions for retrieving guest details from
+the Wait Wait... Don't Tell Me! Stats Page Database.
+"""
